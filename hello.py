41496c4093fa0f768f6d8fd5714008de3d192597
ouiamboujida <ouiamboujida15gmail.com>
2026-05-19 19:36:19 +0100

    fix input problem for Jenkins

--- hello.py
+++ hello.py
@@ -1,14 +1,17 @@
-# hello.py
-print("=" * 40)
+python
+print("========================================")
 print("Bienvenue dans mon premier job Jenkins !")
-print("=" * 40)
+print("========================================")
 
-nom = input("Quel est ton nom ? ")
+nom = "MonSuperEtudiant"
+
 print(f"Bonjour {nom}, ton job Jenkins a réussi !")
 
-# Petit calcul
 a = 10
 b = 5
-print(f"{a} + {b} = {a + b}")
-print(f"{a} - {b} = {a - b}")
+
+print(f"{a} + {b} = {a+b}")
+print(f"{a} - {b} = {a-b}")
+
+print("✅ Tous les tests passent avec succès")
 
